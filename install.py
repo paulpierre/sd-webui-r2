9cd07a6c15778874f3823ff385992637d4fca7ba
Paul Pierre <guitarsmith@gmail.com>
2024-05-28 05:47:36 -0400

    add logging, use process hook, async -> sync

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 import launch
 if not launch.is_installed("r2client"):
     launch.run_pip("install r2client==0.2.1", "r2client: requirements for Cloudflare R2")
-if not launch.is_installed("httpx"):
-    launch.run_pip("install httpx", "httpx: requirements for Cloudflare R2")
+if not launch.is_installed("requests"):
+    launch.run_pip("install requests", "requests: requirements for Cloudflare R2")
--- scripts/sd-webui-r2.py
+++ scripts/sd-webui-r2.py
@@ -1,14 +1,18 @@
+from modules.processing import StableDiffusionProcessing
 from modules import scripts, script_callbacks, shared
 from r2client.R2Client import R2Client
 from modules.shared import opts
 from typing import Optional
+import requests
 import logging
-import httpx
-import os
+import hashlib
 import json
+import os
+
 
 logger = logging.getLogger(__name__)
 
+shared.opts.add_section("r2", "🌩️ R2BucketUpload Settings")
 shared.opts.add_option("r2_access_key_id", shared.OptionInfo("", "R2 Access Key ID", section=("r2", "R2 Settings")))
 shared.opts.add_option("r2_secret_access_key", shared.OptionInfo("", "R2 Secret Access Key", section=("r2", "R2 Settings")))
 shared.opts.add_option("r2_upload_path", shared.OptionInfo("assets", "R2 Upload Path", section=("r2", "R2 Settings")))
@@ -27,23 +31,31 @@ class R2BucketUpload(scripts.Script):
     def show(self, is_img2img):
         return not is_img2img
 
-    async def postprocess(self, p, processed, *args, **kwargs):
+    #async def postprocess(self, p, processed, *args, **kwargs):
+    def process(self, p: StableDiffusionProcessing, *args, **kwargs):  #
+        logger.info("⚡ [R2BucketUpload] Starting post processing ..")
 
         slack_webhook_url = opts.slack_webhook_url
 
         data = p.js()
 
-        if processed.images:
-            output_file_path = processed.images[0]  # Path to the output image
-            output_json_path = output_file_path.replace('.png', '.json')  # Assuming output is a .png file
+        logger.debug(f"⚡ [R2BucketUpload] JSON data: {data}")
+
+        if p.images:
     
+            output_file_path = p.images[0]  # Path to the output image
+            file_hash = self.generate_sha256_file(output_file_path)
+            output_json_path = os.path.join(os.path.dirname(output_file_path), f"{file_hash}.json")
+            
             with open(output_json_path, 'w') as json_file:
                 json.dump(data, json_file)
             
-            logger.info(f"🔄 Uploading {output_file_path} to R2")
-            img_url = self.upload_to_r2(output_file_path)
-            logger.info(f"🔄 Uploading {output_json_path} to R2")
+            logger.info(f"🔄 [R2BucketUpload] Uploading image {output_file_path} to R2")
+            img_url = self.upload_to_r2(output_file_path, file_name=f"{file_hash}.png")
+            
+            logger.info(f"🔄 [R2BucketUpload] Uploading json {output_json_path} to R2")
             prompt_url = self.upload_to_r2(output_json_path)
+            
             # Clean up
             os.remove(output_json_path)
 
@@ -56,13 +68,14 @@ class R2BucketUpload(scripts.Script):
                     data.get('negative_prompt'),
                     data.get('sd_model_name')
                 )
-                await self.send_slack_message(payload, webhook_url=slack_webhook_url)
+                self.send_slack_message(payload, webhook_url=slack_webhook_url)
 
-            logger.info("✅ Successfully uploaded to R2")
+            logger.info("✅ [R2BucketUpload] Successfully uploaded to R2")
+        else:
+            logger.error("❌ [R2BucketUpload] No images found in processed data")
 
-
-    def upload_to_r2(self, file_path):
-        file_name = os.path.basename(file_path)
+    def upload_to_r2(self, file_path, file_name: Optional[str] = None):
+        file_name = file_name or os.path.basename(file_path)
 
         bucket_name = opts.r2_bucket_name or os.environ["R2_BUCKET_NAME"]
         base_upload_path = opts.r2_upload_path or os.environ["R2_UPLOAD_PATH"]
@@ -78,17 +91,30 @@ class R2BucketUpload(scripts.Script):
         )
 
         upload_path = f"{base_upload_path}/{file_name}"
-        logger.info(f"Uploading file to R2 - bucket_name:{bucket_name} file_path:{file_path} upload_path:{upload_path}")
+        logger.info(f"🔄 [R2BucketUpload] Uploading file to R2 - bucket_name:{bucket_name} file_path:{file_path} upload_path:{upload_path}")
         try:
             r2.upload_file(bucket_name, file_path, upload_path)
         except Exception as e:
-            logger.error(f"❌ Failed to upload file to R2: {e}")
+            logger.error(f"❌ [R2BucketUpload] Failed to upload file to R2: {e}")
             return None
 
         url = f"https://{bucket_domain}/{upload_path}"
-        logger.info(f"Uploaded file to R2: {url}")
+
         return url
 
+    def generate_sha256_file(self, file_path):
+        """
+        Generate a SHA256 hash of a file.
+
+        :param file_path: Path to the file to hash
+        :return: SHA256 hash of the file
+        """
+        sha256_hash = hashlib.sha256()
+        with open(file_path, "rb") as file:
+            # Read and update hash in chunks of 4K
+            for byte_block in iter(lambda: file.read(4096), b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
         
     def format_slack_message(
         image_url: str,
@@ -147,16 +173,16 @@ class R2BucketUpload(scripts.Script):
         return blocks
 
 
-    async def send_slack_message(self, payload: dict, webhook_url: Optional[str] = None):
+    def send_slack_message(self, payload: dict, webhook_url: Optional[str] = None):
         webhook_url = webhook_url or opts.slack_webhook_url or os.environ["SLACK_WEBHOOK_URL"]
         if webhook_url is None:
             raise ValueError("Webhook URL is required to send a Slack message.")
 
         # Send Slack notification
-        async with httpx.AsyncClient() as client:
-            result = await client.post(webhook_url, json=payload)
-        print(f"slack response: {result.text}")
+        result = requests.post(webhook_url, json=payload)
+        logger.debug(f"💬 [R2BucketUpload] Slack response: {result.text}")
         return result
+
 
     def on_ui_tabs():
         script_callbacks.on_after_process(R2BucketUpload())
